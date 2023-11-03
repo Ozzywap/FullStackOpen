0b8710505f07d90821b2b887479f72b0e9611bd2
Osman Haji <osman6191@outlook.com>
2023-11-03 20:39:56 +1100

    used destructuring

--- part01/exercises/courseInfo/src/App.jsx
+++ part01/exercises/courseInfo/src/App.jsx
@@ -1,25 +1,25 @@
-const Header = (props) => {
+const Header = ({course}) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{course}</h1>
     </div>
   )
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <div>
-      <Part part={props.parts[0]}/>
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
+      <Part part={parts[0]}/>
+      <Part part={parts[1]}/>
+      <Part part={parts[2]}/>
     </div>
   )
 }
 
-const Part = (props) => {
+const Part = ({part}) => {
   return (
     <div>
-      <p>{props.part.name} {props.part.exercises}</p>
+      <p>{part.name} {part.exercises}</p>
     </div>
   )
 }
