b744f70fc49ad729fea3cd169d6beb3fdd04da03
Osman Haji <osman6191@outlook.com>
2023-11-13 21:20:16 +1100

    completed exercises 2.18 to 2.20 and country data app

--- part02/exercises/countryData/src/App.jsx
+++ part02/exercises/countryData/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const App = () => {
+const App = ({ api_key }) => {
   const [country, setCountry] = useState("");
   const [countryData, setCountryData] = useState(null);
   const [countryList, setCountryList] = useState(null);
+  const [countryWeather, setCountryWeather] = useState(null);
 
   useEffect(() => {
     axios
@@ -12,7 +13,6 @@ const App = () => {
       .then((response) => {
         const cl = response.data.map((country) => country.name.common);
         setCountryList(cl);
-        console.log("successfully fetched country list");
       })
       .catch(() => {
         console.log("failed at fetching country list");
@@ -25,28 +25,88 @@ const App = () => {
         countryName.toLowerCase().includes(country)
       );
       if (countriesToDisplay.length === 1) {
-        axios
-          .get(
-            `https://studies.cs.helsinki.fi/restcountries/api/name/${countriesToDisplay[0]}`
-          )
-          .then((response) => {
-            const countryObject = {
-              name: response.data.name.common,
-              capital: response.data.capital,
-              area: response.data.area,
-              languages: response.data.languages,
-              flag: response.data.flags.png,
-            };
-            setCountryData(countryObject);
-          })
-          .catch(() => {
-            console.log("failed at fetching country");
-          });
+        fetchCountryData(countriesToDisplay[0]);
+      } else {
+        setCountryData(null);
       }
     }
   }, [country]);
 
+  const fetchCountryData = (country) => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+      .then((response) => {
+        // console.log(response.data.capital);
+        // console.log(response.data.capitalInfo.latlng);
+        const countryObject = {
+          name: response.data.name.common,
+          capital: response.data.capital,
+          area: response.data.area,
+          languages: response.data.languages,
+          flag: response.data.flags.png,
+        };
+        setCountryData(countryObject);
+        fetchCountryWeather(
+          response.data.capital,
+          response.data.capitalInfo.latlng
+        );
+      })
+      .catch(() => {
+        console.log("failed to fetch country data");
+      });
+  };
+
+  const fetchCountryWeather = (name, latlng) => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latlng[0]}&lon=${latlng[1]}&appid=${api_key}`
+      )
+      .then((response) => {
+        const countryWeatherObject = {
+          name: name,
+          temperature: response.data.main.temp,
+          wind: response.data.wind.speed,
+          icon: response.data.weather[0].icon,
+        };
+        setCountryWeather(countryWeatherObject);
+        console.log(countryWeatherObject);
+      })
+      .catch(() => {
+        console.log("failed to fetch weather data");
+      });
+  };
+
   const renderCountryData = () => {
+    if (countryData !== null && countryWeather != null) {
+      return (
+        <div>
+          <div>
+            <h1>{countryData.name}</h1>
+            <p>capital: {countryData.capital}</p>
+            <p>area: {countryData.area}</p>
+            <h4>languages:</h4>
+            <ul>
+              {Object.values(countryData.languages).map((language, idx) => {
+                return <li key={idx}>{language}</li>;
+              })}
+            </ul>
+            <img
+              src={countryData.flag}
+              alt={`${countryData.name}'s flag`}
+            ></img>
+          </div>
+          <div>
+            <h1>Weather in {countryWeather.name}</h1>
+            <p>temperature {countryWeather.temperature} Celsius</p>
+            <img
+              src={`https://openweathermap.org/img/wn/${countryWeather.icon}@2x.png`}
+            ></img>
+            <p>wind {countryWeather.wind} m/s</p>
+          </div>
+        </div>
+      );
+    }
+
     if (country && countryList) {
       const countriesToDisplay = countryList.filter((countryName) =>
         countryName.toLowerCase().includes(country)
@@ -58,28 +118,14 @@ const App = () => {
         return (
           <ul>
             {countriesToDisplay.map((country, idx) => (
-              <p key={idx}>{country}</p>
+              <p key={idx}>
+                {country}{" "}
+                <button onClick={() => fetchCountryData(country)}>show</button>
+              </p>
             ))}
           </ul>
         );
       }
-    }
-
-    if (countryData !== null) {
-      return (
-        <div>
-          <h1>{countryData.name}</h1>
-          <p>capital: {countryData.capital}</p>
-          <p>area: {countryData.area}</p>
-          <h4>languages:</h4>
-          <ul>
-            {Object.values(countryData.languages).map((language, idx) => {
-              return <li key={idx}>{language}</li>;
-            })}
-          </ul>
-          <img src={countryData.flag} alt={`${countryData.name}'s flag`}></img>
-        </div>
-      );
     }
   };
 
--- part02/exercises/countryData/src/main.jsx
+++ part02/exercises/countryData/src/main.jsx
@@ -2,4 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const api_key = import.meta.env.VITE_SOME_KEY;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App api_key={api_key} />
+);
