d2528ba051ee2a0ff05f5d782be6a7202692d38e
Osman Haji <osman6191@outlook.com>
2023-11-27 20:46:15 +1100

    implemented minimax

--- part02/exercises/tictactoe/src/App.jsx
+++ part02/exercises/tictactoe/src/App.jsx
@@ -149,6 +149,82 @@ const Game = () => {
   );
 };
 
+const minimax = (gameState) => {
+  const terminal = terminalState(gameState);
+  if (terminal[0]) {
+    return terminal[1];
+  }
+
+  if (player(gameState)) {
+    let value = -Infinity;
+    const possibleActions = actions(gameState);
+    for (let i = 0; i < possibleActions.length; i++) {
+      value = Math.max(value, minimax(result(gameState, possibleActions[i])));
+    }
+    return value;
+  } else {
+    let value = Infinity;
+    const possibleActions = actions(gameState);
+    for (let i = 0; i < possibleActions.length; i++) {
+      value = Math.min(value, minimax(result(gameState, possibleActions[i])));
+    }
+    return value;
+  }
+};
+
+const result = (gameState, action) => {
+  const newState = gameState.slice();
+  player(gameState) ? (newState[action] = "X") : (newState[action] = "O");
+  return newState;
+};
+
+const actions = (gameState) => {
+  return gameState.reduce((acc, pos, i) => {
+    if (pos === null) {
+      acc.push(i);
+    }
+    return acc;
+  }, []);
+};
+
+const player = (gameState) => {
+  const moveNumber = gameState.reduce((acc, x) => {
+    if (x !== null) {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
+  return moveNumber % 2 === 0;
+};
+
+const terminalState = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      if (squares[a] === "X") {
+        return [true, 1];
+      } else {
+        return [true, -1];
+      }
+    }
+  }
+  if (squares.some((pos) => pos === null)) {
+    return [false, null];
+  } else {
+    return [true, 0];
+  }
+};
+
 const calculateWinner = (squares) => {
   const lines = [
     [0, 1, 2],
